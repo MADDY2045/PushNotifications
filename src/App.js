f129fd24704d50e3b48c3d903cd5c7278f58ba2a
madhavan <madhavaneee08@gmail.com>
2020-08-07 18:31:59 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
+
 const App = () => {
+
+  const [innerWidth,setInnerWidth] = useState(window.innerWidth);
+  const [innerHeight,setInnerHeight] = useState(window.innerHeight);
+useEffect(()=>{
+  window.onresize=()=>{
+    console.clear();
+    let changedwidth = window.innerWidth;
+    let changedheight = window.innerHeight;
+    setInnerWidth(changedwidth);
+    setInnerHeight(changedheight)
+    console.log("changed",changedwidth);
+  }
+},[])
+
+let height=  innerHeight - 120;
+let width  = innerWidth - 100;
   return (
-    <div>
-      <h1>Test </h1>
+    <div style={{background:"blue",height:innerHeight}} >
+    <h1>{`width: ${width}px ,height: ${height}px`}</h1>
+    <div className="row">
+      <div className="col-md-10 card" style={{background:"white",width,height,position:"absolute",left:"30px"}}>
+        test
+      </div>
+    </div>
     </div>
   );
 }
