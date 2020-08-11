184166eefc49e4ea3d8a7de708b49468f30d7777
madhavan <madhavaneee08@gmail.com>
2020-08-11 21:40:56 +0530

    f

--- public/cached_pages.js
+++ public/cached_pages.js
@@ -3,11 +3,48 @@ const cacheName = 'v1';
 
 const cacheAssets=[
     'custom.js',
-    'index.html'
-
+    'index.html',
+    '../src/App.css',
+    '../src/App.js',
+    '../src/components/About.js',
+    '../src/components/Home.js',
+    '../package.json',
+    '../package-lock.json',
+    '../node_modules',
+    '../src/index.js'
 ]
 // install event listener
-self.addEventListener('install', (e)=>{console.log(`service worker installed...`)});
+self.addEventListener('install', (e)=>{
+    console.log(`service worker installed...`)
+    e.waitUntil(
+        caches.open(cacheName).then(cache=>{
+            console.log(`caching files...`);
+            cache.addAll(cacheAssets);
+        }).then(()=>{self.skipWaiting()})
+    )
+});
 
 // Activate event listener
-self.addEventListener('activate', (e)=>{console.log(`service worker activated...`)});
+self.addEventListener('activate', (e)=>{
+    console.log(`service worker activated...`)
+    //remove unwanted caches
+    e.waitUntil(
+        caches.keys().then(cacheNames=>{
+            return Promise.all(
+                cacheNames.map(cache=>{
+                    if(cache !== cacheName){
+                        console.log('service worker clearing cache ....');
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    )
+});
+
+//call fetch event
+self.addEventListener('fetch', (e)=>{
+    console.log('Service Worker fetching .....');
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))
+    )
+})
